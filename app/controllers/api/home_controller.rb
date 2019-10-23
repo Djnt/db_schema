aa1c7b8d3b8b50277b6a2b3c4a3d45ff60b1e238
Djnt <dijientt@gmail.com>
2019-10-23 10:24:05 +0300

    trash

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -30,4 +30,25 @@ class Api::HomeController < ApplicationController
   # sad asdasdasdasdasdasd
   # sad asdasdasdasdasdasd
 
+  def index
+    respond_to :html, :json
+    @users = User.all #filter followed users & add serializer
+    order = ["ASC", "DESC"].include?(params[:order]) ? params[:order] : "ASC"
+    # add scope
+    @tweats = Tweat.all.where("tweats.user_id IN (SELECT target FROM follows WHERE follows.user_id = #{current_user.id})").order("created_at #{order}").map{|t| t.to_json}
+  end
+  def index
+    respond_to :html, :json
+    @users = User.all #filter followed users & add serializer
+    order = ["ASC", "DESC"].include?(params[:order]) ? params[:order] : "ASC"
+    # add scope
+    @tweats = Tweat.all.where("tweats.user_id IN (SELECT target FROM follows WHERE follows.user_id = #{current_user.id})").order("created_at #{order}").map{|t| t.to_json}
+  end
+
+  # sad asdasdasdasdasdasd
+  # sad asdasdasdasdasdasd
+  # sad asdasdasdasdasdasd
+  # sad asdasdasdasdasdasd
+  # sad asdasdasdasdasdasd
+
 end
