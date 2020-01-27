62f7132c2122c256c808084554b4c95f0b5785e0
Djnt <dijientt@gmail.com>
2020-01-27 14:39:05 +0200

    ...

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -9,4 +9,20 @@ class Api::HomeController < ApplicationController
     @tweats = Tweat.all.where("tweats.user_id IN (SELECT target FROM follows WHERE follows.user_id = #{current_user.id})").order("created_at #{order}").map{|t| t.to_json}
   end
 
+  def index
+    respond_to :html, :json
+    @users = User.all #filter followed users & add serializer
+    order = ["ASC", "DESC"].include?(params[:order]) ? params[:order] : "ASC"
+    # add scope
+    @tweats = Tweat.all.where("tweats.user_id IN (SELECT target FROM follows WHERE follows.user_id = #{current_user.id})").order("created_at #{order}").map{|t| t.to_json}
+  end
+  def index
+    respond_to :html, :json
+    @users = User.all #filter followed users & add serializer
+    order = ["ASC", "DESC"].include?(params[:order]) ? params[:order] : "ASC" asddddddd
+    # add scope
+    @tweats = Tweat.all.where("tweats.user_id IN (SELECT target FROM follows WHERE follows.user_id = #{current_user.id})").order("created_at #{order}").map{|t| t.to_json}
+  end
+
+  "ASSSSSSSSSssssssdadsasdasdasdddddddddddddddddddddd"
 end
