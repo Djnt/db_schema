c0431a48361b2c5f50c70171ee0906856af87b15
Djnt <dijientt@gmail.com>
2020-09-02 17:09:14 +0300

    test3

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -27,6 +27,17 @@ class Api::ProfileController < ApplicationController
       tweats = user.tweats
       render json: {email: user.email, followers: followers, follows: follows, tweats: user.tweats}
     end
+
+        #TEST2
+        def show
+          respond_to :html, :json
+          user = User.find(params[:id])
+          # add scopes
+          followers = User.where("id IN (SELECT user_id from follows where follows.target = #{user.id})")
+          follows = User.where("id IN (SELECT target from follows where follows.user_id = #{user.id})")
+          tweats = user.tweats
+          render json: {email: user.email, followers: followers, follows: follows, tweats: user.tweats}
+        end
   
 end
   
