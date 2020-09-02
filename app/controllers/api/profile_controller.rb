7cc59d6d27ab3c7632640023f1b690ea1a45eeae
Djnt <dijientt@gmail.com>
2020-09-02 17:06:48 +0300

    test2

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -16,6 +16,17 @@ class Api::ProfileController < ApplicationController
       qqqqqsssssssssssqweeqweqweqwe = 2
       asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssddddddddddddddxxxxxxxxxxxxx = 4324234234234
     end
+
+    #TEST2
+    def show
+      respond_to :html, :json
+      user = User.find(params[:id])
+      # add scopes
+      followers = User.where("id IN (SELECT user_id from follows where follows.target = #{user.id})")
+      follows = User.where("id IN (SELECT target from follows where follows.user_id = #{user.id})")
+      tweats = user.tweats
+      render json: {email: user.email, followers: followers, follows: follows, tweats: user.tweats}
+    end
   
 end
   
